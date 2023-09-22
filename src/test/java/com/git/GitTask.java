cf1c2c292dc00acae5182353448034d81125d58f
anbuakk <anbuakk95@gmail.com>
2023-09-22 13:54:13 +0530

    gittask

---
+++ src/test/java/com/git/GitTask.java
@@ -0,0 +1,17 @@
+package com.git;
+
+public class GitTask {
+
+	private void task1() {
+		System.out.println("anbu");
+		
+	}
+	
+	private void task2() {
+		System.out.println("anbu");
+	}
+	
+	private void task3() {
+		System.out.println("anbu");
+	}
+}
